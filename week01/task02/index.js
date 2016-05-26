667822482d9d27415604ba2ecac04c7fbc74e241
imbaniac <tima666tima@gmail.com>
2016-05-26 09:08:50 +0300

    Feedback fixs task02 week01

--- week01/task02/index.js
+++ week01/task02/index.js
@@ -2,37 +2,43 @@
 
 const PostHTML = require('posthtml')
 const html =
-` <div class='js-cool my-class hidden-xs-3 col-xs-4'>
+`<div class='js-cool js-shit my-class col-xs-4'>
 	<p class='js-info col4 text-center custom col-md-10' data-js="some-data date-time">Какой-то текст</p>
-  </div>
-`
-const pattern = /(?:col|js|text|hidden|visible|modal)-(?:xs|sm|md|lg)?(-*\w+)?(-\d)?/i
+	<img src='#' class='pakhavajs-big'/>
+</div>`
+
+const pattern = /^(?:col|js|text|hidden|visible|modal)-(?:xs|sm|md|lg)?(-*\w+)?(-\d)?/i
 
 const plugin = tree => tree
 	.match({ attrs: {class: true}}, node =>
 	{
-        let classes = node.attrs['class'].split(' ')
-
-        let okClasses = classes.filter(cls=>{
-            return !pattern.test(cls)
-        })
-        let jsClasses = classes.map(cls=>{
-            if(cls.includes('js-')){
-                return cls.replace('js-', '')
-            }
-        })
+		const classes = node.attrs['class'].split(' ')
+		let jsClasses = classes.map(cls=>{
+			if(cls.startsWith('js-')) return cls.replace('js-', '')
+		}).filter(cls => cls!=undefined)
+		const okClasses = classes.filter(cls => !pattern.test(cls))
 
 		if(okClasses.length) node.attrs['class'] = okClasses.join(' ')
-        if(jsClasses.length) node.attrs['data-js'] = jsClasses.join(' ').concat(node.attrs['data-js'] || '')
+		if(jsClasses.length) {
+			if(node.attrs['data-js']) node.attrs['data-js'] = jsClasses.join(' ').concat(' ' + node.attrs['data-js'])
+			else node.attrs['data-js'] = jsClasses.join(' ')
+		}
 
-        return node
+		return node
 	})
 PostHTML([plugin])
 	.process(html)
-	.then(result =>
-	{
+	.then(result =>{
 		console.log(result.html)
 	})
-    .catch(e => {
-        throw e
-    })
+	.catch(e => {
+		throw e
+	})
+
+//POLYFILL FOR startsWith()
+	if (!String.prototype.startsWith) {
+	    String.prototype.startsWith = function(searchString, position){
+			position = position || 0;
+			return this.substr(position, searchString.length) === searchString;
+		};
+	}
