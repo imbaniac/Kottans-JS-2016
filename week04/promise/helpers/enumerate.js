673a4da781a55db27c35c8c64c2d171920421a0a
imbaniac <tima666tima@gmail.com>
2016-07-05 17:55:16 +0300

    Refactoring ExtendedPromise

--- week04/promise/helpers/enumerate.js
+++
@@ -1,8 +0,0 @@
-function* enumerate(iterable) {
-    let index = 0;
-    for (let iterator of iterable) {
-        yield [index++, iterator];
-    }
-}
-
-module.exports = enumerate;
--- week04/promise/index.js
+++ week04/promise/index.js
@@ -1,39 +1,25 @@
 'use strict';
 
-function* enumerate(iterable) {
-	let index = 0;
-	for (let iterator of iterable) {
-		yield [index++, iterator];
-	}
-}
-
-const isIterable = object =>
-object != null && typeof object[Symbol.iterator] === 'function';
-
 class ExtendedPromise extends Promise {
 
 	static map(input, mapper) {
-		return new this((resolve, reject) => {
-			this.resolve(input).then(iterable => {
-				let pendingPromises = 0;
-				let results = [];
-				if (!isIterable(iterable)) {
-					reject(new TypeError('should be a Promise<Iterable>'));
-				}
-				for (let iterator of iterable) {
-					pendingPromises++;
-					this.resolve(iterator).then(iterator => {
-						this.resolve(mapper(iterator)).then(value => {
-							results.push(value);
-							if (!--pendingPromises) {
-								resolve(results);
-							}
-						}, reject);
-					}, reject);
-				}
-			}, reject);
-		});
-	}
+        return new this((resolve, reject) => {
+            this.resolve(input).then(iterable => {
+                let pendingPromises = 0;
+                let results = [];
+                if (!isIterable(iterable)) reject(new TypeError("Promis should be iterable"));
+                for (let iterator of iterable) {
+                    pendingPromises++;
+                    this.resolve(iterator).then(iterator => {
+                        this.resolve(mapper(iterator)).then(value => {
+                            results.push(value);
+                            if (!pendingPromises - 1) resolve(results);
+                        }, reject);
+                    }, reject);
+                }
+            }, reject);
+        });
+    }
 
 	static some(input, count) {
 		return new this((resolve, reject) => {
@@ -42,36 +28,52 @@ class ExtendedPromise extends Promise {
 				let rejected = [];
 				for (let promise of input) {
 					this.resolve(promise).then(value => {
-						if (resolved.length < count) {
-							resolved.push(value);
-						}
-						if (resolved.length == count) {
-							resolve(resolved);
-						}
+						if (resolved.length < count) resolved.push(value);
+						if (resolved.length == count) resolve(resolved);
 					}, error => {
 						rejected.push(error);
-						if (rejected.length) {
-							reject(rejected);
-						}
+						if (rejected.length) reject(rejected);
 					});
 				}
 			}, reject);
 		});
 	}
 
-	static reduce(input, reducer) {
-		if (typeof fn !== "function") {
-			return;
-		}
-		return function(val) {
-			val = Array.isArray(val) ? val : [val]
-			return val.reduce(function(promise, curr) {
-				return promise.then(function(prev) {
-					return reducer(prev, curr)
-				})
-			}, Promise.resolve(input))
-		}
-	}
+	static reduce(input, reducer, initialValue) {
+        const isInitialValue = arguments.length > 2;
+        let acc;
+        return new this((resolve, reject) => {
+            this.resolve(initialValue).then(initialValue => {
+                this.resolve(input).then(input => {
+                    if (!isIterable(input)) {
+                        if (!isInitialValue) throw new Error;
+						else resolve(initialValue);
+                    }
+                    let promiseAcc = this.resolve(initialValue);
+					let length = 0;
+                    input.map((value, id) => {
+                        length++;
+                        this.resolve(value).then(value => {
+                            if (!id && !isInitialValue) {
+                                promiseAcc = this.resolve(value);
+                                return;
+                            }
+                            promiseAcc = promiseAcc.then(acc => {
+                                return this.resolve(reducer(acc, value, id, length));
+                            }, reject);
+                            if (id == length	- 1) {
+                                promiseAcc.then(resolve, reject);
+                            }
+                        }, reject);
+                    })
+                    if (length === 0) resolve(initialValue);
+                }, reject);
+            }, reject);
+        });
+    }
 }
+
+const isIterable = object =>
+	object != null && typeof object[Symbol.iterator] === 'function';
 
 module.exports = ExtendedPromise;
